72c5675e4694ccbea705abf2fe69654fd58f2a80
wanghao2020 <w786058404@163.com>
2015-09-08 22:15:27 +0800

    get the naive bayes the prior probability

--- Bayes/bayes.py
+++ Bayes/bayes.py
@@ -45,10 +45,44 @@ def setOfwords2Vec(vocablist, inputSet):
 
     return returnVec
 
-#naive bayes train
+# train to get the prior probability
 def trainNB0(trainMatrix, trainCategory):
 
     numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+
+    # 1 probability
+    Pa = sum(trainCategory) / float(numTrainDocs)\
+
+    #get each element probability
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0AllNum = 0.0
+    p1AllNum = 0.0
+
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1AllNum += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0AllNum += sum(trainMatrix[i])
+
+    p1Vec = p1Num / p1AllNum  # change log
+    p0Vec = p0Num / p0AllNum
+
+    return Pa, p1Vec, p0Vec
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Bayes/main.py
+++ Bayes/main.py
@@ -12,10 +12,20 @@ import bayes
 
 postingList, classVec = bayes.loadDataSet()
 
+# get the vablist
 vablist = bayes.createVocablist(postingList)
-
 print "Show my vablist\n", vablist
 
+# get the returnVec
 returnVec = bayes.setOfwords2Vec(vablist, ["my", "love", "dog", "happy", "daddy"])
-
 print "the word vec is ", returnVec
+
+# get the prior probability
+trainMat = []
+for one in postingList:
+    trainMat.append(bayes.setOfwords2Vec(vablist, one))
+
+pa, p1Vec, p0Vec = bayes.trainNB0(trainMat, classVec)
+print "the 1 probability is %f, " % pa
+print "the each class , echa element probability\n", p1Vec, '\n', p0Vec
+
